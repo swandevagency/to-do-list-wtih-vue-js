0bcbbe06f93b010f9773929eb45b2bba6d6bda35
Arsam <arsam5ba5@gmail.com>
2022-06-16 14:34:23 +0430

    some functionality added

--- to do list/src/views/HomeView.vue
+++ to do list/src/views/HomeView.vue
@@ -1,19 +1,18 @@
 <script>
   import searchIcon from '../components/icons/search-icon.vue'
   import closeIcon from '../components/icons/close-icon.vue'
-  import AddTask from '../components/AddTask.vue'
+  import up from '../components/icons/up.vue'
   
   export default {
     data() {
         return {
           add: true,
-
         }
     },
     components: {
-      AddTask,
       searchIcon,
-      closeIcon
+      closeIcon,
+      up
     },
     methods: {
       onclick(){
@@ -24,6 +23,16 @@
         console.log('close');
         let closableDiv = document.querySelector('.task-container')
         closableDiv.remove()
+      },
+      onScroll(){
+        document.querySelector('#getStarted').scrollIntoView({
+          behavior: 'smooth',
+        });
+      },
+      upClick(){
+        document.querySelector('header').scrollIntoView({
+          behavior: 'smooth',
+        });
       }
     }
     
@@ -35,13 +44,13 @@
     <div class="left-part-of-hero">
       <h1>Company Name To Do List</h1>
       <p>have discipline in your life</p>
-      <a>Get Started</a>
+      <button @click="onScroll()">Get Started</button>
     </div>
     <div class="right-part-of-hero">
       
     </div>
   </hero>
-  <Section class="list-section">
+  <Section class="list-section" id="getStarted">
     <div class="list-section-wrapper">
 
       <div class="introduction-part">
@@ -56,54 +65,73 @@
         </button>
         
       </div>
-      <input class="task-search-bar" type="text" placeholder="Search for Task...">
+      <input  class="task-search-bar" type="text" placeholder="Search for Task...">
       <searchIcon />
-    
-      <div class="task-container">
-        <div>
-          <div class="task-first-part">
-            <h4>TITLE</h4>
-            <button @click="closeClick()"><closeIcon /></button>
-          </div>
-          <p class="task-description">Lorem  consectetur adipiscing elit Lorem ipsum dolor sit amet, elit elit consectetur adipiscing elit Lorem ipsum dolor sit amet, elit elit consectetur adipiscing elit Lorem ipsum dolor sit amet, elit elit consectetur adipiscing elit</p>
-          <div class="task-last-part">
-            <p>TASK 1</p>
-            <p>09:00 PM</p>
+      
+
+      <section>
+        <div class="section" v-if="!add">
+          <div class="add-task-container">
+                <label>Title:</label>
+                <input v-model="title" type="text" class="titleInput" />
+
+                <label>Description:</label>
+                <input v-model="description" class="descriptionInput">
+                <button @click="onclick()" class="add-task-button">Add</button>
           </div>
         </div>
-      </div>
+      </section>
 
-      <div class="task-container">
-        <div>
-          <div class="task-first-part">
-            <h4>TITLE</h4>
-            <button @click="closeClick()"><closeIcon /></button>
-          </div>
-          <p class="task-description">Lorem ipsum dolor sit amet, elorem tur adipiscing elit Lorem ipsum dolor sit amet, elit elit consectetur adipiscing elit Lorem ipsum dolor sit amet, elit elit consectetur adipiscing elit</p>
-          <div class="task-last-part">
-            <p>TASK 1</p>
-            <p>12:30 PM</p>
+
+      <section>  
+        <div class="task-container">
+          <div>
+            <div class="task-first-part">
+              <h4>TITLE</h4>
+              <button @click="closeClick()"><closeIcon /></button>
+            </div>
+            <p class="task-description">Lorem consectetur adipiscing elit Lorem ipsum dolor sit amet, elit elit consectetur adipiscing elit Lorem ipsum dolor sit amet, elit elit consectetur adipiscing elit Lorem ipsum dolor sit amet, elit elit consectetur adipiscing elit</p>
+            <div class="task-last-part">
+              <p>TASK 1</p>
+              <p>09:00 PM</p>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div class="task-container">
-        <div>
-          <div class="task-first-part">
-            <h4>TITLE</h4>
-            <button @click="closeClick()"><closeIcon /></button>
-          </div>
-          <p class="task-description">Lorem ipsum dolor amet, elit elit consectetur amet, elit elit consectetur adipiscing elit Lorem ipsum dolor sit amet, elit elit consectetur adipiscing elit</p>
-          <div class="task-last-part">
-            <p>TASK 1</p>
-            <p>14:50 PM</p>
+        <div class="task-container">
+          <div>
+            <div class="task-first-part">
+              <h4>TITLE</h4>
+              <button @click="closeClick()"><closeIcon /></button>
+            </div>
+            <p class="task-description">Lorem ipsum dolor sit amet, elorem tur adipiscing elit Lorem ipsum dolor sit amet, elit elit consectetur adipiscing elit Lorem ipsum dolor sit amet, elit elit consectetur adipiscing elit</p>
+            <div class="task-last-part">
+              <p>TASK 1</p>
+              <p>12:30 PM</p>
+            </div>
           </div>
         </div>
-      </div>
 
+        <div class="task-container">
+          <div>
+            <div class="task-first-part">
+              <h4>TITLE</h4>
+              <button @click="closeClick()"><closeIcon /></button>
+            </div>
+            <p class="task-description">Lorem ipsum dolor amet, elit elit consectetur amet, elit elit consectetur adipiscing elit Lorem ipsum dolor sit amet, elit elit consectetur adipiscing elit</p>
+            <div class="task-last-part">
+              <p>TASK 1</p>
+              <p>14:50 PM</p>
+            </div>
+          </div>
+        </div>
+      </section>
 
     </div>
-  </Section>  
+  </Section> 
+  <button @click="upClick()" class="up-button">
+    <up />
+  </button> 
 </template>
 
 <style scoped lang="scss">
@@ -144,16 +172,18 @@ hero{
     font-size: 22px;
   }
 
-  a{
+  button{
     background-color: #FFC85D;
     margin-top: 20px;
     padding: 8px 15px;
     font-weight: 800;
     border-radius: 5px;
+    outline: none;
+    border: none;
     color: #004655;
     box-shadow: 2px 2px #d5d5d5;
   }
-  a:hover{
+  button:hover{
     box-shadow: none;
   }
 }
@@ -220,13 +250,15 @@ hero{
     border: none;
     outline: none;
     border-radius: 40px;
+    background-color: #dff6ff;
     font-size: 14px;
     padding: 15px 75px;
-    box-shadow: 0px 4px 4px rgb(0 0 0 / 25%);
-    margin-bottom: 100px;
+    box-shadow: 1px 1px 5px #00000085;
+;
+    margin-bottom: 50px;
     position: relative;
     &::placeholder{
-      color:#b5b5b5;
+      color:#868686;
       font-weight: 100;
     }  
   }
@@ -237,6 +269,76 @@ hero{
     left: 95px;
     margin-top: 12px;
 }
+
+
+
+
+
+
+.section{
+  width: 100%;
+  margin-bottom: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  label{
+    color: #004655;
+    font-weight: 500;
+    font-size: 18px;
+  }
+}
+
+.add-task-container{
+  padding: 50px 30px;
+  border-radius: 10px;
+  background-color: hsl(197deg 100% 94%);
+  width: 100%;
+  height: 45vh;
+  display: flex;
+  flex-direction: column;
+  box-shadow: inset 1px 1px 6px #00000065;
+}
+
+.titleInput{
+  background-color: #00D1FF;
+  padding: 8px 15px;
+  border-radius: 5px;
+  border: none;
+  box-shadow: 2px 2px #00000030;
+  margin-bottom:10px;
+  color: #004655;
+  font-size: 18px;
+  font-weight: 600;
+}
+
+.descriptionInput{
+  background-color: #00D1FF;
+  padding: 8px 15px;
+  border-radius: 5px;
+  border: none;
+  box-shadow: 2px 2px #00000030;
+  height:100px;
+  margin-bottom:20px;
+  color: #004655;
+  font-size: 18px;
+  font-weight: 600;
+}
+
+.add-task-button{
+  background-color: #FFC85D;
+  padding: 8px 15px;
+  font-weight: 600;
+  border-radius: 5px;
+  border: none;
+  color: #004655;
+  box-shadow: 2px 2px #00000030;
+}
+
+
+
+
+
 
 .task-container{
   width: 100%;
@@ -288,6 +390,21 @@ hero{
       font-weight:600;
     }
   }
+}
+
+.up-button[data-v-9ea40744] {
+    display: flex;
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    z-index: 10;
+    position: fixed;
+    bottom: 20px;
+    right: 20px;
+    border: none;
+    background-color: #ffc85d;
+    align-items: center;
+    justify-content: center;
 }
 
 </style>
